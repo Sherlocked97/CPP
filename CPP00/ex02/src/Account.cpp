bfffcf03a938018fa0ce5df079dd1dcbe178bdc2
mhedtman <mhedtman@student.42heilbronn.de>
2022-11-05 09:20:43 +0100

    final form

--- CPP00/ex02/src/Account.cpp
+++ CPP00/ex02/src/Account.cpp
@@ -121,7 +121,7 @@ Account::~Account(void)
 	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex;
 	std::cout << ";amount:" << this->_amount ;
-	std::cout << ";closed";
+	std::cout << ";closed" << std::endl;
 	return ;
 }
 
