13ec2e9d79a42b4c2c0d4f43f54b3f4718815e22
mhedtman <mhedtman@student.42heilbronn.de>
2022-11-16 10:39:46 +0100

    CPP04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -2,16 +2,50 @@
 #include <string>
 #include <fstream>
 
+
+int	throwError(int errorCode)
+{
+	if (errorCode == 1)
+		std::cout << "Wrong ammount of args!" << std::endl;
+	if (errorCode == 2)
+		std::cout << "Error while opening files!" << std::endl;
+	return (errorCode);
+}
+
 int	main(int argc, char **argv)
 {
-	std::ifstream infile;
-	std::ofstream outfile;
+	std::ifstream 	infile;
+	std::ofstream 	outfile;
+	std::size_t		begin = 0;
+	std::string		s1, s2, newOutfile, line;
 
 	if (argc != 4)
-	{
-		std::cout << "Wrong ammount of args!" << std::endl;
-		return (1);
-	}
+		return (throwError(1));
+	newOutfile.append(argv[1]);
+	newOutfile.append(".replace");
 	infile.open(argv[1]);
-	
+	outfile.open(newOutfile);
+	if (!outfile.is_open() || !infile.is_open())
+		return (throwError(2));
+	s1 = argv[2];
+	s2 = argv[3];
+	while (getline(infile, line))
+	{
+		begin = 0;
+		while (begin < line.npos)
+		{
+			begin = line.find(s1, begin+s2.length());
+			if (begin != line.npos)
+			{
+				std::cout << begin << std::endl;
+				line.erase(begin, s1.length());
+				line.insert(begin, s2);
+			}
+		}
+		outfile << line << '\n';
+
+	}
+	infile.close();
+	outfile.close();
+	return (0);
 }
